9b01d29df049c805c9c2efa5e899210e5e4ed9bd
Jose Sanabria <mljosesanabria@gmail.com>
2023-05-04 21:19:47 -0400

    cambio package 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/calendarapp_frontend/'
+  plugins: [react()]
 })
